fd6e6331e59a6b63333f104c85080498bd34ab1a
jxl00125 <jiang_mmcc@163.com>
2018-11-06 14:44:36 +0800

    Update demo2_ImproveTest.java

--- src/test/java/HS/demo2_ImproveTest.java
+++ src/test/java/HS/demo2_ImproveTest.java
@@ -3,11 +3,11 @@ package HS;
 import org.junit.Test;
 
 public class demo2_ImproveTest {
-
+// 100以内斐波那契数列变种: 能被3整除或该数含有数字3(比如13,30等)输出Buzz,能被5整除或该数含5输出Fizz,上面两种情况都包含输出FizzBuzz
     @Test
     public void main() {
         for (int i = 1; i <= 100; i++) {
-            if (i%15 ==0 || i/10 == 5 || i%10 ==5 || i/10 == 3 || i%10 ==3 ){
+            if (i%15 ==0 || ( i/10 == 5 || i%10 ==5 && i/10 == 3 || i%10 ==3 ) ){
                 System.out.println("FizzBuzz");
             }
             else if (i%5 == 0 ) System.out.println("Buzz");
